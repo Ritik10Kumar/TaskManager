2c9428bd100333b874c7b0e48461d58e4c15d9cc
Ritik10Kumar <ritik10kumar@gmail.com>
2025-01-07 21:34:32 +0530

    changes in database creadentials

--- config/database.js
+++ config/database.js
@@ -4,8 +4,8 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres", // Replace with your PostgreSQL username
   host: "localhost",
-  database: "task_manager", // Replace with your database name
-  password: "password", // Replace with your PostgreSQL password
+  database: "taskmanager", // Replace with your database name
+  password: "Admin@12345", // Replace with your PostgreSQL password
   port: 5432,
 });
 
